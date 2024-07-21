b0636a70556583a129ecc453a31df8fe96d7f120
Uliana Shlagina <shlaginau@gmail.com>
2024-07-22 01:15:43 +0700

    corrected

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,5 +15,8 @@ export default [
         version: "detect" // Automatically detect the react version
       }
     }
+  },
+  {
+    ignores: ["build/**", "node_modules/**", "dist/**", "coverage/**"]
   }
 ];
